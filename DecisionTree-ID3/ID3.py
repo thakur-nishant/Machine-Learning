ee7475db5801cf71c850593b7f53b0990d53ba47
Nishant Thakur <nt.nishantt@gmail.com>
2018-02-10 02:04:42 -0600

    completed my ugly code for ID3
    need to work on visual represemtation

--- DecisionTree-ID3/ID3.py
+++ DecisionTree-ID3/ID3.py
@@ -3,19 +3,19 @@ import math
 class ID3:
     def __init__(self):
         self.votes = []
-        with open('playball-attr.txt') as f:
+        with open('attributes.txt') as f:
             for line in f:
                 self.attributes = line[:-1].split(',')
         # print(self.attributes)
-        with open('playball.txt') as f:
+        with open('house-votes-84.data') as f:
             for line in f:
                 data = line[:-1].split(',')
                 self.votes.append(data)
 
-
+        self.visted = []
         self.stat = self.statistics(self.votes, self.attributes)
 
-        self.eS = self.entropy(self.stat, attr = self.attributes[-1])
+        self.eS = self.entropy(self.stat, attr = self.attributes[0])
         print("Entropy(S) =", self.eS)
         # for i in self.votes:
         #     print(i)
@@ -25,24 +25,55 @@ class ID3:
 
         select = (max(gain, key=gain.get))
         print("selected:",select)
+        self.visted.append(select)
+        ind = self.attributes.index(select)
         for key in self.stat[select].keys():
             # print(self.stat)
             if key != 'total':
                 self.eS = self.entropy(self.stat[select][key], None)
                 print("Entropy(S-"+key+") =", self.eS)
 
-                if self.eS != 0:
+                if abs(self.eS) != 0:
                     newV = []
                     for i in self.votes:
-                        if i[0] == key:
+                        if i[ind] == key:
                             newV.append(i)
 
                     stat1 = self.statistics(newV, self.attributes)
                     gain1 = self.gain(self.eS, stat1)
                     print(gain1)
+                    if gain1:
+                        select1 = (max(gain1, key = gain1.get))
+                        print("selected:", select1)
+                        self.visted.append(select1)
+                        ind1 = self.attributes.index(select1)
 
-                    select1 = (max(gain1, key=gain1.get))
-                    print("selected:", select1)
+                        for key1 in stat1[select1]:
+                            # print(self.stat)
+                            if key1 != 'total':
+                                print(key1, stat1[select1][key1])
+                                self.eS1 = self.entropy(stat1[select1][key1], None)
+                                print("Entropy(S-" + key1 + ") =", self.eS1)
+
+                                if abs(self.eS1) != 0:
+                                    newV1 = []
+                                    for i in newV:
+                                        if i[ind1] == key1:
+                                            newV1.append(i)
+
+                                    stat11 = self.statistics(newV1, self.attributes)
+                                    gain11 = self.gain(self.eS1, stat11)
+                                    print(gain11)
+                                    if gain11:
+                                        select11 = (max(gain11, key=gain11.get))
+                                        self.visted.append(select11)
+                                        print("selected:", select11)
+                                    else:
+                                        break
+                                else:
+                                    for d in stat1[select1][key1]:
+                                        if d != 'total':
+                                            print("Play: ", d)
 
                 else:
                     for d in self.stat[select][key].keys():
@@ -59,25 +90,26 @@ class ID3:
 
         for vote in votes:
             for j in attributes:
-                key = attributes.index(j)
-                if vote[key] in data[attributes[key]]:
-                    if vote[-1] in data[attributes[key]][vote[key]]:
-                        data[attributes[key]][vote[key]][vote[-1]] += 1
+                if j not in self.visted:
+                    key = attributes.index(j)
+                    if vote[key] in data[attributes[key]]:
+                        if vote[0] in data[attributes[key]][vote[key]]:
+                            data[attributes[key]][vote[key]][vote[0]] += 1
+                        else:
+                            data[attributes[key]][vote[key]][vote[0]] = 1
+                        data[attributes[key]][vote[key]]['total'] += 1
                     else:
-                        data[attributes[key]][vote[key]][vote[-1]] = 1
-                    data[attributes[key]][vote[key]]['total'] += 1
-                else:
-                    data[attributes[key]][vote[key]] = {}
-                    data[attributes[key]][vote[key]]['total'] = 1
-                    data[attributes[key]][vote[key]][vote[-1]] = 1
-                data[attributes[key]]['total'] += 1
-        print(len(attributes),data)
+                        data[attributes[key]][vote[key]] = {}
+                        data[attributes[key]][vote[key]]['total'] = 1
+                        data[attributes[key]][vote[key]][vote[0]] = 1
+                    data[attributes[key]]['total'] += 1
+        print(len(data.keys()),data)
         return  data
 
 
     def entropy(self, data, attr):
         entropy = 0
-        if attr == self.attributes[-1]:
+        if attr == self.attributes[0]:
             for key in data[attr]:
                 if key != 'total':
                     p = data[attr][key]['total']/data[attr]['total']
@@ -93,7 +125,7 @@ class ID3:
     def gain(self, S, data):
         gain = {}
         for col in data:
-            if col != 'Play':
+            if col != 'Class' and col not in self.visted:
                 gain[col] = S
                 for key in data[col]:
                     if key != 'total':
--- DecisionTree-ID3/ID3v1.py
+++ DecisionTree-ID3/ID3v1.py
@@ -12,17 +12,50 @@ class ID3():
                 data = line[:-1].split(',')
                 self.votes.append(data)
 
+        self.visted = []
+        self.stat = self.statistics(self.votes, self.attributes)
 
-    def entropy(self, attr):
+        self.eS = self.entropy(self.stat, self.attributes[-1])
+        print("Entropy(S) =", self.eS)
+
+
+    def entropy(self, data, attr):
         entropy = 0
-        for key in attr:
-            if key != 'total':
-                p = attr[key] / attr['total']
-                entropy += p * math.log2(p)
-
+        if attr == self.attributes[-1]:
+            for key in data[attr]:
+                if key != 'total':
+                    p = data[attr][key]['total']/data[attr]['total']
+                    entropy += p * math.log2(p)
+        else:
+            for key in data:
+                if key != 'total':
+                    p = data[key]/data['total']
+                    entropy += p * math.log2(p)
         return -entropy
 
 
-    def statistic(self, ):
+    def statistics(self, votes, attributes):
+        data = {}
+
+        for attr in attributes:
+            data[attr] = {}
+            data[attr]['total'] = 0
+
+        for vote in votes:
+            for j in attributes:
+                key = attributes.index(j)
+                if vote[key] in data[attributes[key]]:
+                    if vote[-1] in data[attributes[key]][vote[key]]:
+                        data[attributes[key]][vote[key]][vote[-1]] += 1
+                    else:
+                        data[attributes[key]][vote[key]][vote[-1]] = 1
+                    data[attributes[key]][vote[key]]['total'] += 1
+                else:
+                    data[attributes[key]][vote[key]] = {}
+                    data[attributes[key]][vote[key]]['total'] = 1
+                    data[attributes[key]][vote[key]][vote[-1]] = 1
+                data[attributes[key]]['total'] += 1
+        print(len(attributes),data)
+        return  data
 
 
